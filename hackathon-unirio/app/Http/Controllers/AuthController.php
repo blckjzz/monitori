9b0c285422057e2528157967d4ecb8b3d5afdfa4
Diego Cerqueira <xdiegocerqueira@gmail.com>
2017-05-07 07:07:44 -0300

    fix authcontroller

--- hackathon-unirio/app/Http/Controllers/AuthController.php
+++ hackathon-unirio/app/Http/Controllers/AuthController.php
@@ -17,26 +17,17 @@ class AuthController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-
     public function login(Request $request)
     {
         $credentials = $request->only(['matricula', 'password']);
-        Auth:
+        try {
+            $token = JWTAuth::attempt($credentials);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Erro na geração de token'], 500);
+        }
+        if (!$token) {
+            return response()->json(['error' => 'Login inválido'], 401);
+        }
+        return response()->json(compact('token'));
     }
-
-    /*
-     public function login(Request $request)
-     {
-         $credentials = $request->only(['matricula', 'password']);
-         try {
-             $token = JWTAuth::attempt($credentials);
-         } catch (JWTException $e) {
-             return response()->json(['error' => 'Erro na geração de token'], 500);
-         }
-         if (!$token) {
-             return response()->json(['error' => 'Login inválido'], 401);
-         }
-         return response()->json(compact('token'));
-     }
-    */
 }
