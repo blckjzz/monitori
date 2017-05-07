0dccd8010d4a706fbde6e6d51dd925f7682622b5
Diego Cerqueira <xdiegocerqueira@gmail.com>
2017-05-07 02:27:11 -0300

    api add route

--- hackathon-unirio/routes/api.php
+++ hackathon-unirio/routes/api.php
@@ -14,12 +14,15 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => 'jwt.auth'], function () {
+
+    Route::get('user/auth','UserController@showAuthUser');
     Route::resource('user', 'UserController');
     Route::resource('curso','CursoController');
     Route::resource('disciplina','DisciplinaController');
     Route::resource('monitoria','MonitoriaController');
     Route::post('/disciplina/{id}/teach', 'DisciplinaController@teach');
     Route::post('/disciplina/{id}/unteach', 'DisciplinaController@unteach');
+
 });
 
 Route::post('/login', 'AuthController@login');
